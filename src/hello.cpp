d04e9db6151afe50f925ddc4b6190ab7423dfd2c
Irina Baeva <irina.baeva@bonial.com>
2021-09-29 15:48:23 +0200

    Modify hello file to send header info

--- src/hello.cpp
+++ src/hello.cpp
@@ -6,7 +6,7 @@ char *PrintHello(char *socket_type)
     if (socket_type == "server")
     {
 
-        return "Hello from server!!!";
+        return "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
     }
     else
     {
